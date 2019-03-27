b65196d93846f65a32b6536078c7583e8309e68c
Tyler Steiman <tsteiman@switchboxinc.com>
2019-03-27 13:03:50 -0400

    Bind keys via a defun so it can be reused

--- defuns.el
+++ defuns.el
@@ -4,3 +4,10 @@
   (interactive)
   (move-end-of-line 1)
   (newline-and-indent))
+
+(defun my/bind-keys-list (keys)
+  "Bind keys based on a passed list (see keys.el)"
+  (dolist (key keys)
+    (let ((binding (car key))
+          (command (car (cdr key))))
+      (global-set-key (kbd binding) command))))
--- keys.el
+++ keys.el
@@ -1,29 +1,26 @@
 ;; set some keybindings
-(dolist (key '(
-               ("C-c m s"   magit-status)
-               ("C-c m b"   magit-blame)
-               ("C-x C-b"   ibuffer)
-               ("C-c a j"   ace-jump-char-mode)
-               ("C-j"       emmet-expand-line)
-               ("C-c h f"   browse-url-of-file)
-               ("C-]"       xref-find-definitions)
-               ("C-t"       xref-pop-marker-stack)
-               ;; helm bindings
-               ("C-x C-f"   helm-find-files)
-               ("C-x b"     helm-buffers-list)
-               ("M-x"       helm-M-x)
-               ("C-c s s"   helm-swoop)
-               ("C-c h i"   helm-imenu)
-               ("C-c h t"   helm-etags-select)
-               ;; spotify bindings
-               ("C-c s p n" spotify-next)
-               ("C-c s p p" spotify-playpause)
-               ("C-c s p q" spotify-quit)
-               ;; custom defun bindings
-               ("C-o"       my/new-line-below)
-               ))
-  (let ((binding (car key))
-        (command (car (cdr key))))
-    (global-set-key (kbd binding) command)))
+(my/bind-keys-list '(
+                     ("C-c m s"   magit-status)
+                     ("C-c m b"   magit-blame)
+                     ("C-x C-b"   ibuffer)
+                     ("C-c a j"   ace-jump-char-mode)
+                     ("C-j"       emmet-expand-line)
+                     ("C-c h f"   browse-url-of-file)
+                     ("C-]"       xref-find-definitions)
+                     ("C-t"       xref-pop-marker-stack)
+                     ;; helm bindings
+                     ("C-x C-f"   helm-find-files)
+                     ("C-x b"     helm-buffers-list)
+                     ("M-x"       helm-M-x)
+                     ("C-c s s"   helm-swoop)
+                     ("C-c h i"   helm-imenu)
+                     ("C-c h t"   helm-etags-select)
+                     ;; spotify bindings
+                     ("C-c s p n" spotify-next)
+                     ("C-c s p p" spotify-playpause)
+                     ("C-c s p q" spotify-quit)
+                     ;; custom defun bindings
+                     ("C-o"       my/new-line-below)
+                     ))
 
 (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
