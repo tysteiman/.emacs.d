7e9057fa32f0565604cb7b380b793ee4c0fb3075
Tyler Steiman <ma.steiman@gmail.com>
2019-03-31 12:33:44 -0400

    Set emacs hook for C-c e -> eval buffer & message

--- hooks.el
+++ hooks.el
@@ -1,1 +1,10 @@
+;; configure code buffers (that extend prog-mode) to default settings
 (add-hook 'prog-mode-hook 'my/configure-code-buffer)
+
+;; add emacs-lisp-mode hooks
+(add-hook 'emacs-lisp-mode-hook (lambda ()
+                                  ;; set local keybinding for C-c e -> eval buffer & message
+                                  (local-set-key (kbd "C-c e") (lambda ()
+                                                                 (interactive)
+                                                                 (eval-buffer)
+                                                                 (message "Buffer evaluted.")))))
